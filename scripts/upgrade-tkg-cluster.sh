2444eefab353aaccddf624a30b2b49350aca25c7
ubuntu <ubuntu@ubuntu-269.haas-269.pez.pivotal.io>
2020-08-01 15:44:20 -0700

    updates from 1st Aug

--- scripts/upgrade-tkg-cluster.sh
+++ scripts/upgrade-tkg-cluster.sh
@@ -1,2 +1,1 @@
-#available versions: [v1.16.12+vmware.1-tkg.1.da7afe7 v1.17.7+vmware.1-tkg.1.154236c v1.16.8+vmware.1-tkg.3.60d2ffd v1.17.8+vmware.1-tkg.1.5417466]
 tkg upgrade cluster k8-demo-cluster-1 --namespace mission-partner-1 --kubernetes-version v1.17.8+vmware.1-tkg.1.5417466
--- scripts/vsphere-kubectl-login.sh
+++ scripts/vsphere-kubectl-login.sh
@@ -1,1 +1,2 @@
 kubectl vsphere login --server=10.193.221.65 --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify
+cp ~/.kube/config /home/ubuntu/.kube-tkg/config
